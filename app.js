32a5b158c0a868ab3d07f1a30662cf63539118e8
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2019-04-12 19:29:15 -0600

    removed auto-message

--- app.js
+++ app.js
@@ -119,37 +119,6 @@ client.on('guildMemberUpdate',(oMember, nMember) => {
     oMember.guild.channels.find(x => x.name === 'action-log').send(nickembed);
   });
 
-// // Ban //
-// client.on('guildBanAdd', (guild, user) => {
-
-//     //const usericon = guild.user.displayAvatarURL;
-//     const message = Discord.RichEmbed;
-
-//     let banembed = new Discord.RichEmbed()
-//     .setColor('#292F33')
-//     .setDescription(`🏴 **${user.username}** has been \`banned\` from the server.`)
-//     .setFooter(`user Ban`, ``).setTimestamp(message.createdAt);
-
-//     guild.channels.find(x => x.name === 'action-log').send(banembed);
-
-//}); 
-// UnBan //
-//client.on("guildBanRemove", (guild, user) => {
-//
-  //  const usericon = user.user.displayAvatarURL;
-//    const message = Discord.RichEmbed;
-//
-//    let unbanembed = new Discord.RichEmbed()
-//   .setColor("#E1E8ED")
-//   .setDescription(`🏳 **${user.usertag}** has been \`unbanned\` from the server.`)
-//    .setFooter(`user Ban`, ``).setTimestamp(message.createdAt);
-//
-//    user.guild.channels.find("name", "action-log").send(unbanembed);
-//
-//});
-
-////////////////////////////////////
-
 // Perms Stuff
 
 client.on('message', async message => {
@@ -168,54 +137,5 @@ client.on('message', async message => {
     if (commandfile) commandfile.run(client,message,args);
 
 });
-
-    // Auto Message //
-    var NOTIFY_CHANNEL;
-    client.on('ready', () => {
-        NOTIFY_CHANNEL = client.channels.find('id', '397928222555242501'); // Channel to send notification
-    });
     
-    let replies = [
-        //":gear: |  **Tip:** text"
-        //":gear: |  **Tip:** After your permanent invite link gets 10 unique uses you will receive the **Inviter** role! Type \`>invite\` to view your permanent invite link!",
-        ":gear: |  **Tip:** You can join a private one on one voice channel with any other member on this server.", 
-        ":gear: |  **Tip:** You can report a user with the command `>report @user reason`.",
-        ":gear: |  **Tip:** If you need help with commands just type `>help` and check your DM's!",
-        ":gear: |  **Tip:** You can watch videos through discord by clicking on them.",
-        ":gear: |  **Tip:** Type `>invite` to view your permanent invite link.",
-        ":gear: |  **Tip:** You can view users information with `>userinfo @user` as well as server information `>serverinfo` and, bot information `>botinfo`.",
-        ":gear: |  **Tip:** If you want to recive updates for GraalOnline Classic type the command `>goc`. If you want to stop reciving updates just type the command again.",
-        ":gear: |  **Tip:** Use the command `>suggest Suggestion` to send a suggestion to the servers staff!",
-        ":gear: |  **Tip:** If you want to change your colour use the command `?ranks` to view your options.",
-        ":gear: |  **Tip:** To mute channels on mobile, press and hold on the desired channel you would like to mute, on PC right click the channel you want muted, if you want to mute the whole server do the action on the server icon.",
-        ":gear: |  **Tip:** You can use HTML format within Graal guild chat. You must format your brackets like so: `<\\text>`",
-        ":gear: |  **Tip:** Subscribe to VBanished on youtube!\nhttps://www.youtube.com/user/VBanished",
-        ":gear: |  **Tip:** Typing `unstick me` in feedback will instantly warp you back to Angel Clan.",
-        ":gear: |  **Tip:** If you need any extra help with a command just use add 'help' after the command like this: `>help <command>`"
-    
-    ];
-
-  //  let result = Math.floor((Math.random() * replies.length));
-
-    const START_DATE = '2018-10-10'; // Date used as the starting point for multi-hour intervals, must be YYYY-MM-DD format
-    const START_HOUR = 19; // Hour of the day when the timer begins (0 is 12am, 23 is 11pm), used with START_DATE and INTERVAL_HOURS param
-    const INTERVAL_HOURS = 37; // Trigger at an interval of every X hours
-    const TARGET_MINUTE = 30; // Minute of the hour when the chest will refresh, 30 means 1:30, 2:30, etc.
-    const OFFSET = 0; // Notification will warn that the target is X minutes away
-    
-    // Don't change any code below
-    const NOTIFY_MINUTE = (TARGET_MINUTE < OFFSET ? 60 : 0) + TARGET_MINUTE - OFFSET;
-    const START_TIME = new Date(new Date(START_DATE).getTime() + new Date().getTimezoneOffset() * 60000 + START_HOUR * 3600000).getTime();
-    
-    setInterval(function() {
-        var d = new Date();
-        let result = Math.floor((Math.random() * replies.length));
-        if(Math.floor((d.getTime() - START_TIME) / 3600000) % INTERVAL_HOURS > 0) return; // Return if hour is not the correct interval
-        if(d.getMinutes() !== NOTIFY_MINUTE) return; // Return if current minute is not the notify minute
-        NOTIFY_CHANNEL.send(replies[result]);
-    }, 60 * 1000); // Check every minute
-
-    //message react
-    
-
 client.login(process.env.BOT_TOKEN);
