c25b5a2c3ef05506df0ff2daaa1ca27df8f255d0
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2019-05-18 22:17:11 -0600

    Update setnick.js

--- commands/setnick.js
+++ commands/setnick.js
@@ -8,19 +8,21 @@ module.exports.run = async (client, message, args) => {
     if(!message.member.hasPermission('MANAGE_NICKNAMES')) return errors.noPerms(message, 'MANAGE_NICKNAMES');
 
     let nUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!nUser) return message.reply("Please specify a user that is on this server.");
+    if(!nUser) return message.channel.send(`**${message.author.username}**, Please specify a user that is on this server.`);
 
     let nickname = args.join(" ").slice(22);
     if(!nickname) return message.reply("Please specify a name.");
 
-    if(nUser.hasPermission('MANAGE_NICKNAMES')) return message.channel.send("That person can't have their name changed.")
+    if(nUser.hasPermission('MANAGE_NICKNAMES')) return message.channel.send("That person can't have their name changed.");
     
     message.delete();
 
     await(nUser.setNickname(nickname));
+    return message.channel.send(`Their nickname was successfully changed!`);
 
 
 };
 module.exports.help = {
-    name: 'setnick'
+    name: 'setnick',
+    aliases: ['']
 };
